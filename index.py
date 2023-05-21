c1f7b4d5438edd53edd7c70619451fff101b3340
Andre Horbach <44560466+andrehorbach@users.noreply.github.com>
2023-05-21 15:56:22 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,53 +1,43 @@
-
 import random
 import json
 import requests
 import os
- 
+
 # Category List for Quotes
-category = ['inspirational', 'attitude', 'dreams',
-            'experience', 'intelligence', 'leadership', 'success']
-api_url = 'https://api.api-ninjas.com/v1/\
-quotes?category={}'.format(category[random.randint(0, 6)])
- 
+api_url = 'https://api.api-ninjas.com/v1/facts?limit=1'
 # Enter your API Key Here
 response = requests.get(api_url, headers=
-                        {'X-Api-Key': 'I76dbvtaZtxpvp1+nUnDkA==gKbH7pWRiKqyWsxG'})
- 
+						{'X-Api-Key': 'XXXXXXXXXXXXXXXXXX'})
+
 if response.status_code == requests.codes.ok:
-    quote = response.text
-    # Load the response into a json object
-    quotes = json.loads(quote)
-    q = quotes[0]['quote']
-    # In case of receiving multiple quotes,
-    # we will pick the first one
-    mainQuote = q.split('.')[0]
+	quote = response.text
+	mainQuote = quote
 else:
-    print("Error:", response.status_code, response.text)
- 
- 
+	print("Error:", response.status_code, response.text)
+
+
 # Reading the readme file
 with open("README.md", mode="r", encoding="utf8") as f:
-    readmeText = f.read()
- 
+	readmeText = f.read()
+
 # Finding the tag where the quote is to be replaced
 openingTag = "<h4 quote"
 closingTag = "</h4 quote"
- 
+
 startIndex = readmeText.index(openingTag)
 endIndex = readmeText.index(closingTag)
- 
+
 quoteMarkdown = "<h4 quote align='center'>" + mainQuote + "." + "</h4 quote>"
- 
+
 content = readmeText[startIndex +
-                     len(openingTag): endIndex]
+					len(openingTag): endIndex]
 newContent = (
-    readmeText[:startIndex]
-    + quoteMarkdown
-    + readmeText[endIndex + len(closingTag) + 1:]
+	readmeText[:startIndex]
+	+ quoteMarkdown
+	+ readmeText[endIndex + len(closingTag) + 1:]
 )
- 
+
 # Writing new Quote into readme file
 readme_file = open("README.md",
-                   mode="w", encoding="utf8")
+				mode="w", encoding="utf8")
 readme_file.write(newContent)
